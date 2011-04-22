89e71ee2f08b6d9b4ff0d09163f4c190d45c1f71
Andreas Bogk <andreas@andreas.org>
2011-04-22 19:13:36 +0200

    file i/o wrapper

--- main.ml
+++ main.ml
@@ -1,7 +1,38 @@
 open Batteries_uni
 open Print
+open String
+open File
+open IO
+
 open Allparser
 
-match parse_hex_string ('<'::'4'::'5'::'4'::'1'::'>'::[]) with
-| SomeE s   -> printf p"{%{char list}}" (fst s)
-| NoneE err -> printf p"{%{char list}}" err
+let list_of_string s =
+  let rec list_of_string_aux acc s i =
+    if i = 0 then
+      s.[i]::acc
+    else
+      list_of_string_aux (s.[i]::acc) s (pred i)
+  in
+    list_of_string_aux [] s (pred (length s));;
+
+let string_of_list l =
+  let s = create (List.length l) in
+  let rec string_of_list_aux ll i =
+    match ll with
+    | head::tail -> begin s.[i] <- head; string_of_list_aux tail (succ i) end
+    | [] -> s
+  in
+    string_of_list_aux l 0;;
+
+let infile = open_in "foo.input" in
+let data = read_all infile in
+begin
+  close_in infile;
+  match parse_hex_string (list_of_string data) with
+  | SomeE s   -> let outfile = open_out "foo.output" in
+                 begin
+                   nwrite outfile (string_of_list (fst s));
+                   close_out outfile
+                 end
+  | NoneE err -> Print.printf p"%s\n" (string_of_list err)
+end
